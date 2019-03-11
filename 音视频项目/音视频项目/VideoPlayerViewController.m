847ea7b7431715a57db216ac643372e27208bc1a
王丹 <1653591454@qq.com>
2019-03-11 10:13:49 +0800

    视频UI界面基本搭建

--- 音视频项目/音视频项目/VideoPlayerViewController.m
+++ 音视频项目/音视频项目/VideoPlayerViewController.m
@@ -7,8 +7,26 @@
 //
 
 #import "VideoPlayerViewController.h"
+#import <Masonry.h>
 
 @interface VideoPlayerViewController ()
+
+
+/**
+ 内容View
+ */
+@property (weak,nonatomic)UIView *contentView;
+
+/**
+ 播放View
+ */
+@property (weak,nonatomic)UIView *playerView;
+
+
+/**
+ 播放按钮
+ */
+@property (weak,nonatomic)UIButton *playerBtn;
 
 @end
 
@@ -17,16 +35,58 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self setupUI];
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma 设置UI界面
+- (void)setupUI {
+    
+    
+    UIView *contentView = [UIView new];
+    self.contentView = contentView;
+    [self.view addSubview:contentView];
+    self.contentView.backgroundColor = [UIColor redColor];
+    
+    UIView *playerView = [UIView new];
+    self.playerView = playerView;
+    [self.contentView addSubview:playerView];
+    self.playerView.backgroundColor = [UIColor greenColor];
+    
+    UIButton *playerBtn = [UIButton buttonWithType:0];
+    self.playerBtn = playerBtn;
+    [self.contentView addSubview:playerBtn];
+    self.playerBtn.backgroundColor = [UIColor orangeColor];
+    [playerBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [playerBtn setTitle:@"Play" forState:UIControlStateNormal];
+    [playerBtn addTarget:self action:@selector(actionPlayBtn:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.contentView mas_makeConstraints:^(MASConstraintMaker *make) {
+       
+        make.top.mas_equalTo(self.view.mas_safeAreaLayoutGuideTop);
+        make.leading.trailing.mas_equalTo(self.view);
+        make.height.mas_equalTo(250);
+    }];
+    
+    [self.playerView mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.top.leading.trailing.mas_equalTo(self.contentView);
+        make.height.mas_equalTo(220);
+    }];
+    
+    [self.playerBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.playerView.mas_centerX);
+        make.centerY.mas_equalTo(self.playerView.mas_centerY);
+    }];
+    
+    
+    
 }
-*/
+
+- (void)actionPlayBtn:(UIButton *)sender {
+    
+    NSLog(@"Play");
+}
 
 @end
